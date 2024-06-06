2eb5439abaa6db024c66b96d119b1f15d72e2981
João Benedet <105388117+joaobenedetmachado@users.noreply.github.com>
2024-06-06 15:23:13 -0300

    Update firebase-config.js

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -1,13 +1,13 @@
 import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyC1b6r3_Dh4_MxayB48DWP0FM20431Xvl0",
-  authDomain: "chatglobaldb.firebaseapp.com",
-  projectId: "chatglobaldb",
-  storageBucket: "chatglobaldb.appspot.com",
-  messagingSenderId: "678100347346",
-  appId: "1:678100347346:web:5c293808d98d299501c00b",
-  measurementId: "G-19JW0WL018"
+  apiKey: "AIzaSyB6Mxp1ytVHyIYOIdCFo2fYf7h6QieB9_c",
+  authDomain: "chatglobaldev.firebaseapp.com",
+  projectId: "chatglobaldev",
+  storageBucket: "chatglobaldev.appspot.com",
+  messagingSenderId: "857083942722",
+  appId: "1:857083942722:web:20689695bf1b74797edadb",
+  measurementId: "G-NYCQC30P3Y"
 };
 
 const app = initializeApp(firebaseConfig);
